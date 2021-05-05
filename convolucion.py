319c4f72e34dd484227373ea0d679877052ce6fe
Diego <A01552594@itesm.mx>
2021-05-05 11:56:57 -0500

    Modificar a convolución con padding

--- convolucion.py
+++ convolucion.py
@@ -9,8 +9,6 @@ def conv_helper(fragment, kernel):
             result += fragment[row,col] *  kernel[row,col]
     return result
 
-
-
 image = np.array ([[1,2,3,4,5,6],
               [7,8,9,10,11,12],
               [0,0,1,16,17,18],
@@ -26,22 +24,20 @@ image_row, image_col = image.shape
 kernel_row, kernel_col = kernel.shape
 
 
-outputy=int(image_col-(kernel_col/2)*2+1)
-outputx=int(image_row-(kernel_row/2)*2+1)
+output = np.zeros(image.shape)
 
-output = np.zeros((outputx,outputy))
+pad_height = int((kernel_row - 1) / 2)
+pad_width = int((kernel_col - 1) / 2)
 
+padded_image = np.zeros((image_row + (2 * pad_height), image_col + (2 * pad_width)))
 
+padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
    
-for row in range(outputx):
-    for col in range(outputy):
+for row in range(image_row):
+    for col in range(image_col):
             output[row, col] = conv_helper(
                                 image[row:row + kernel_row, 
                                 col:col + kernel_col],kernel)
 
 
 print(output)
-
-
-
-
